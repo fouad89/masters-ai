44e8bce334c25731d0032d4859d820ba95844c57
Fouad <fouad89ahmed@gmail.com>
2021-09-28 18:46:10 +0100

    nearest neighbor algorithm

--- Term 1/Metaheuristics/01-Lab1/lab1_tsp.py
+++ Term 1/Metaheuristics/01-Lab1/lab1_tsp.py
@@ -2,20 +2,28 @@
 
 """
 import os
+import sys
 import random
 import numpy as np
 # set the seed
 np.random.seed(42)
 
+# uncomment when done
+#file = sys.argv[1]
 
-city_dict = {}
-with open('Term 1/Metaheuristics/01-Lab1/TSP dataset/dummy_data.tsp', 'r') as f:
-    next(f)
-    for line in f:
-        
-        values = line.split()
-        city_dict[int(values[0])] = [float(i) for i in values[1:]]
+def read_file(filename):
+    city_dict = {}
+    with open(filename, 'r') as f:
+        n = f.readline() # the first value of the file gives n the number of cities
+        print(n)
+        for line in f:
+            # loop to read lins and add key:city id, value: x,y coords into dictionary. 
+            values = line.split()
+            city_dict[int(values[0])] = [float(i) for i in values[1:]]
+    return city_dict
 
+file = 'Term 1/Metaheuristics/01-Lab1/TSP dataset/dummy_data.tsp'
+city_dict = read_file(file)
 
 def euc_distance(x1, y1, x2, y2):
     return ((x1-x2)**2 + (y1-y2)**2)**(1/2)
@@ -25,7 +33,7 @@ not_visited = [c_id for c_id in city_dict.keys() if c_id not in visited]
 total_distance = []
 print(f'initial visited list: {visited}')
 print(f'initial not visited list {not_visited}')
-for _ in range(len(city_dict)-1):
+while len(visited) != len(city_dict):
     distances = {}
     for city in not_visited:
         x1, y1 = city_dict[visited[-1]]
@@ -46,7 +54,7 @@ for _ in range(len(city_dict)-1):
 
 print(f"Final Visited: {visited}")
 print(f"Final Not Visited: {not_visited}")
-print(f"Total Distance: {sum(total_distance)}")
+print(f"Total Distance: {round(sum(total_distance))}")
 
 
 
