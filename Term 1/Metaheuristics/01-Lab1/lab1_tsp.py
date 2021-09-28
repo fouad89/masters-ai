8a26835f7280a02b2fc82877823d5afc34343391
Fouad <fouad89ahmed@gmail.com>
2021-09-28 20:07:16 +0100

    alternative method

--- Term 1/Metaheuristics/01-Lab1/lab1_tsp.py
+++ Term 1/Metaheuristics/01-Lab1/lab1_tsp.py
@@ -8,7 +8,7 @@ import numpy as np
 # set the seed
 np.random.seed(42)
 
-# uncomment when done
+#uncomment when done
 #file = sys.argv[1]
 
 def read_file(filename):
@@ -26,14 +26,14 @@ file = 'Term 1/Metaheuristics/01-Lab1/TSP dataset/dummy_data.tsp'
 city_dict = read_file(file)
 
 def euc_distance(x1, y1, x2, y2):
-    return ((x1-x2)**2 + (y1-y2)**2)**(1/2)
+    return round(((x1-x2)**2 + (y1-y2)**2)**(1/2))
 
 visited = [np.random.choice(list(city_dict.keys()))] # selecting a random city id
 not_visited = [c_id for c_id in city_dict.keys() if c_id not in visited]
 total_distance = []
 print(f'initial visited list: {visited}')
 print(f'initial not visited list {not_visited}')
-while len(visited) != len(city_dict):
+for _ in range(len(city_dict)-1):
     distances = {}
     for city in not_visited:
         x1, y1 = city_dict[visited[-1]]
@@ -42,19 +42,54 @@ while len(visited) != len(city_dict):
         distances[city] = dist
 
     min_dist_id = min(distances, key=distances.get)
-    min_dist = min(distances.values())
-    total_distance.append(min_dist)
+    print(min_dist_id)
     visited.append(min_dist_id)
+    print(distances)
     
     if min_dist_id in not_visited:
         not_visited.remove(min_dist_id)
-    print(f"Visited: {visited}")
-    print(f'not_visited {not_visited}')
-    print('\n')
+    total_distance.append(distances[min_dist_id])
+    # print(f"Visited: {visited}")
+    # print('\n')
+    # print(f'not_visited {not_visited}')
 
-print(f"Final Visited: {visited}")
-print(f"Final Not Visited: {not_visited}")
-print(f"Total Distance: {round(sum(total_distance))}")
+
+
+output_file = 'output.tsp'
+with open('Term 1/Metaheuristics/01-Lab1/TSP dataset/output.tsp','w+') as f:
+    f.writelines(f"{np.sum(total_distance)}\n")
+    f.writelines(f'{i}\n' for i in visited)
+
+
+
+
+# part 2: alternative method
+new_visited = [np.random.choice(list(city_dict.keys()))] # selecting a random city id
+new_not_visited = [c_id for c_id in city_dict.keys() if c_id not in new_visited]
+new_total_distance = []
+print('\n'*3)
+print(f'initial new_visited list: {new_visited}')
+print(f'initial not visited list {new_not_visited}')
+
+for _ in range(len(city_dict)-1):
+    new_distances = {}
+    for i in range(len(city_dict)-1):
+        x1, y1 = city_dict[new_visited[-1]]
+        city_id = np.random.choice(new_not_visited)
+        x2, y2 = city_dict[city_id]
+        dist = euc_distance(x1, y1, x2, y2)
+        new_distances[city_id] = dist
+        new_visited.append(city_id)
+    print(new_distances)
+    
+    if city_id in new_not_visited:
+        new_not_visited.remove(city_id)
+    new_total_distance.append(new_distances[city_id])
+    
+    print(new_visited)
+    print(new_not_visited)
+
+print(new_distances)
 
 
 
